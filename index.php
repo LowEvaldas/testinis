ae42996a91c7e81b1cf1811cd7e02ef6aa4c2835
DESKTOP-NFPHQ3P\Vartotojas <lowevaldas@gmail.com>
2018-02-28 13:29:57 +0200

    Pirmas komitas sukeliam failus.

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Webas</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="menustyle.css">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css?family=Lora|Open+Sans" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+        <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        
+    </head>
+    <body>
+  <header>
+
+    <div class="bt-buy"><a href="#">Pirkti</a></div>
+ 
+</header>
+</body>
+
+</html>
